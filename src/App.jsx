f3707e800c763f5d86e777b96e1bf70971e71e7f
feramanurung4 <132858087+feramanurung4@users.noreply.github.com>
2023-10-28 21:43:34 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -66,6 +66,11 @@ function App() {
       <AddProduct show={showAddProduct} handleClose={handleClose} onCreate={onCreate} />
       <EditProduct show={showEditProduct} handleClose={handleClose} onUpdate={onUpdate} data={data} />
     </div>
+    <div>
+     <footer className="footer">
+        <p>Copyright &copy; 2023 Ferawati Manurung UTS PWL </p>
+      </footer>
+    </div>
   )
 }
 
